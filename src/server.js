1636a659deed2095d4006ee6a004a3a1cb5eaf04
Keegan <keegan.adams15@gmail.com>
2021-02-03 13:31:49 -0600

    minor changes

--- src/server.js
+++ src/server.js
@@ -4,11 +4,13 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, "build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
-  res.sendFile("./index.html");
-});
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
