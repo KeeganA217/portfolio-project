536f1dc2a34478136ac266bd5d9211dfd146be54
Keegan <keegan.adams15@gmail.com>
2021-02-03 15:35:14 -0600

    minor changes

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("build"));
 
   app.use("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "build", "index.html"))
   );
 }
 
