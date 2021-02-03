5653e9287f8211d386e80ad32297c437a52fe26a
Keegan <keegan.adams15@gmail.com>
2021-02-03 13:12:35 -0600

    change

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile("./index.html");
 });
 
 app.listen(port, () => {
